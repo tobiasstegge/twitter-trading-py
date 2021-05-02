c9eb31437667682316f4622fb6fd5cee797b43cc
tobiassteggemann <tobias.steggemann@t-online.de>
2021-05-02 18:42:49 +0200

    init

---
+++ src/twitter.py
@@ -0,0 +1,30 @@
+from tweepy import API, OAuthHandler, TweepError
+
+
+class Twitter:
+    def __init__(self):
+        self.API_KEY = 'vwg4NPXZwm7D2n8bvDSS5Xqoi'
+        self.API_SECRET_KEY = 'jz5GlLoBAxJZJFdVFOQg3SfcwKaaXTYtniyTQxFKBjWBDLW61u'
+        self.ACCESS_TOKEN = '3394473448-dtERpRINiBbmcYHtbSC5aIgG9HvnqyFx5phJp8c'
+        self.ACCESS_TOKEN_SECRET = 'gsrjoDDOP1LPDUnV9sBppIDAkwDKnuYKLG6gKrJOdEONA'
+
+        try:
+            auth = OAuthHandler(self.API_KEY, self.API_SECRET_KEY)
+            auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_TOKEN_SECRET)
+
+            self.client = API(auth)
+
+        except TweepError as e:
+            print(e)
+        else:
+            print(f'Success! You are connected as {self.client.me().screen_name}')
+
+    def get_last_tweet(self):
+        elons_twitter = self.client.get_user('elonmusk')
+        tweet = self.client.user_timeline(id=elons_twitter.id_str, count=1)
+        print(tweet[0].text)
+
+
+if __name__ == '__main__':
+    Twitter().get_last_tweet()
+
